8a5d407187d231ff350d87a55d0f44568a0efc75
Ingridck28 <ingridck@hiof.no>
2026-04-17 13:22:02 +0200

    Update Ingrid.jsx

---
+++ src/components/Ingrid.jsx
@@ -0,0 +1,17 @@
+export default function Ingrid (){
+    return (
+    <article>
+        <img />
+
+        <h2>Ingrid</h2>
+
+        <p>
+            <a href="mailto:ingridck@hiof.no">
+                ingridck@hiof.no
+            </a>
+        </p>
+        <p>Bachelor i Digitale medier og design</p>
+    </article>
+    )
+}
+
