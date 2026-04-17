d099da324061886e78a834d1e81a429c6a495d6b
amandarohne <amandak@hiof.no>
2026-04-17 12:53:43 +0200

    1.utkast

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import Header from './components/Header'
+
+function App() {
+  
+
+  return (
+    <>
+    <Header />
+
+     <main>
+      <p>Arbeidskrav 4</p>
+     </main>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+
+export default function Header (){
+    return (
+        <header>
+            <h1>Gruppe 21</h1>
+        </header>
+    )
+}
+
