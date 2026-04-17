31133ba1b1ca0256bf6541e8edf1ed7f28528c25
amandarohne <amandak@hiof.no>
2026-04-17 13:21:37 +0200

    laget eget branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import Header from './components/Header'
+import Amanda from './components/Amanda'
 
 function App() {
   
@@ -10,6 +11,10 @@ function App() {
 
      <main>
       <p>Arbeidskrav 4</p>
+      
+      <section>
+          <Amanda />
+      </section>
      </main>
     </>
   )
---
+++ src/components/Amanda.jsx
@@ -0,0 +1,15 @@
+export default function Amanda() {
+return(
+<article>
+    <img/>
+    <h2>Amanda</h2>
+    <p>
+        <a href="mailto:amandak@hiof.no">
+            amandak@hiof.no
+        </a>
+    </p>
+    <p>Bachelor i Digitale Medier og Design</p>
+</article>
+)
+}
+
