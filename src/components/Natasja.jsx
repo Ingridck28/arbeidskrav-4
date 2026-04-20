0f639b46a59d9d9d41e923b1cfdf2c0cbef2e7c9
NatasjaKap <mnkaplon@hiof.no>
2026-04-20 12:13:37 +0200

    Natasja
    lagt til komponent Natasja

---
+++ src/components/Natasja.jsx
@@ -0,0 +1,16 @@
+export default function Natasja (){
+    return (
+    <article>
+        <img />
+
+        <h2>Natasja</h2>
+
+        <p>
+            <a href="mailto:mnkaplon@hiof.no">
+                mnkaplon@hiof.no
+            </a>
+        </p>
+        <p>Bachelor i Digitale medier og design</p>
+    </article>
+    )
+}
