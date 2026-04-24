ea4a6f303713358deb1b88c6ab54551064599cfe
Ingridck28 <ingridck@hiof.no>
2026-04-24 22:31:28 +0200

    La til bilde
    Ingrid

--- src/components/Ingrid.jsx
+++ src/components/Ingrid.jsx
@@ -1,9 +1,10 @@
 import '../style/ingrid.css'
+import bilde from '../assets/ingrid.png'
 
 export default function Ingrid (){
     return (
         <article className="ingrid-card">
-             <img />
+             <img src={bilde} alt="Illustrasjon av Ingrid" />
         <h2>Ingrid</h2>
         <a href="mailto:ingridck@hiof.no">ingridck@hiof.no</a>
         <p>Bachelor i Digitale medier og design</p>
