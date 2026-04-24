7f000119d494c2c1740f5e5bec74b1a01c85ae96
Ingridck28 <ingridck@hiof.no>
2026-04-24 11:23:56 +0200

    Ingrid
    Endret til arbeidskrav

---
+++ arbeidskrav-sanity/schemaTypes/arbeidskrav.js
@@ -0,0 +1,17 @@
+export default {
+    name: 'arbeidskrav',
+    type: 'document',
+    title: 'Arbeidskrav',
+    fields: [
+        {
+            name: 'tittel',
+            type: 'string',
+            title: 'Tittel'
+        },
+        {
+            name: 'beskrivelse',
+            type: 'text',
+            title: 'Beskrivelse'
+        }
+    ]
+}
